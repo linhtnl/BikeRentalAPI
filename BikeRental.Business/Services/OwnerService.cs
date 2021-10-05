ed26e57b4987fd273c89fc32883fbadfa9be6737
anhttd <donganh500@gmail.com>
2021-10-06 00:20:52 +0700

    Update OwnerService (GetALL)

--- BikeRental.Business/Services/OwnerService.cs
+++ BikeRental.Business/Services/OwnerService.cs
@@ -2,12 +2,14 @@
 using AutoMapper.QueryableExtensions;
 using BikeRental.Data.Models;
 using BikeRental.Data.Repositories;
+using BikeRental.Data.Responses;
 using BikeRental.Data.UnitOfWorks;
 using BikeRental.Data.ViewModels;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,15 +18,20 @@ namespace BikeRental.Business.Services
     public interface IOwnerService : IBaseService<Owner>
     {
         Task<OwnerViewModel> CreateNew(Owner ownerInfo);
-        OwnerViewModel GetOwnerById(Guid id);
+        Task<OwnerRatingViewModel> GetOwnerById(Guid id);
         Task<Owner> GetOwner(Guid id);
         Task<OwnerViewModel> GetByMail(string mail);
+        Task<List<OwnerRatingViewModel>> GetAll();
     }
     public class OwnerService : BaseService<Owner>, IOwnerService
     {
         private readonly IConfigurationProvider _mapper;
-        public OwnerService(IUnitOfWork unitOfWork, IOwnerRepository repository, IMapper mapper) : base(unitOfWork, repository)
+        private readonly IBikeService _bikeService;
+        private readonly IFeedbackService _feedbackService;
+        public OwnerService(IUnitOfWork unitOfWork,IBikeService bikeService,IFeedbackService feedbackService, IOwnerRepository repository, IMapper mapper) : base(unitOfWork, repository)
         {
+            _bikeService = bikeService;
+            _feedbackService = feedbackService;
             _mapper = mapper.ConfigurationProvider;
         }
 
@@ -34,9 +41,9 @@ namespace BikeRental.Business.Services
             return owner;
         }
 
-        public OwnerViewModel GetOwnerById(Guid id)
+        public async Task<OwnerRatingViewModel> GetOwnerById(Guid id)
         {
-            return Get(x => x.Id.Equals(id)).ProjectTo<OwnerViewModel>(_mapper).FirstOrDefault();
+            return await Get(x => x.Id.Equals(id)).ProjectTo<OwnerRatingViewModel>(_mapper).FirstOrDefaultAsync();
         }
 
         public async Task<OwnerViewModel> GetByMail(string mail)
@@ -56,6 +63,28 @@ namespace BikeRental.Business.Services
             {
                 return null;
             }
+        }
+        public async Task<List<OwnerRatingViewModel>> GetAll()
+        {
+            double? rating = 0;
+            int total = 0;
+            var owners = Get(o => o.Bikes != null).ProjectTo<OwnerRatingViewModel>(_mapper);
+            List<OwnerRatingViewModel> listOwner = owners.ToList();
+            if (listOwner.Count == 0) throw new ErrorResponse((int)HttpStatusCode.NotFound, "Can not found");
+            for (int i = 0; i < listOwner.Count; i++)
+            {
+                var listBike = await _bikeService.GetBikeByOwnerId(listOwner[i].Id);
+                listOwner[i].NumberOfBikes = listBike.Count();
+                foreach (var bike in listBike)
+                {
+                    var tempRating = await _feedbackService.GetBikeRating(bike.Id);
+                    total += tempRating.FirstOrDefault().Key;
+                    rating += tempRating.FirstOrDefault().Value;
+                }
+                listOwner[i].Rating = rating;
+                listOwner[i].NumberOfRatings = total;
+            }
+            return listOwner;
         }
     }
 }
---
+++ BikeRental.Data/ViewModels/OwnerRatingViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeRental.Data.Models
+{
+    public class OwnerRatingViewModel
+    {
+        public Guid Id { get; set; }
+        public string PhoneNumber { get; set; }
+        public string IdentityNumber { get; set; }
+        public string Fullname { get; set; }
+        public string Address { get; set; }
+        public int NumberOfBikes { get; set; }
+        public string IdentityImg { get; set; }
+        public double? Rating { get; set; }
+        public int? NumberOfRatings { get; set; }
+        public Guid? AreaId { get; set; }
+    }
+}
