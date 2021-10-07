246e07cec8e1819010e74aae2781d1018302b3fa
Nguyen Lam Nhat Tien <tien20001810@gmail.com>
2021-10-08 01:04:36 +0700

    Finish token service

---
+++ BikeRental.Data/ViewModels/TokenViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeRental.Data.ViewModels
+{
+    public class TokenViewModel
+    {
+        public TokenViewModel(Guid id, int role)
+        {
+            Id = id;
+            Role = role;
+        }
+        public Guid Id { get; set; }
+        public int Role { get; set; }
+    }
+}
