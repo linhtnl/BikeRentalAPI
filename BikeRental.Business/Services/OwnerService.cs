7099598f5d9ea8d48bb4c262bdeccd10131694a4
anhttd <donganh500@gmail.com>
2021-10-12 10:38:44 +0700

    update OwnerService

--- BikeRental.Business/Services/OwnerService.cs
+++ BikeRental.Business/Services/OwnerService.cs
@@ -32,6 +32,7 @@ namespace BikeRental.Business.Services
         Task<Owner> GetOwner(Guid id);
         Task<OwnerViewModel> GetByMail(string mail);
         Task<DynamicModelResponse<OwnerRatingViewModel>> GetAll(OwnerRatingViewModel model, int filterOption, int pageNum);
+        Task<List<OwnerByAreaViewModel>> GetListOwnerByAreaId(Guid id);
     }
     public class OwnerService : BaseService<Owner>, IOwnerService
     {
@@ -176,7 +177,7 @@ namespace BikeRental.Business.Services
         {
             //only admin
             var owner = await GetAsync(id);
-            if (owner == null) throw new ErrorResponse((int)HttpStatusCode.BadRequest, "Bike not found");
+            if (owner == null) throw new ErrorResponse((int)HttpStatusCode.BadRequest, "Can not found");
             owner.Status = (int)UserStatus.Deactive;
             owner.BanTimes++;
             await UpdateAsync(owner);
@@ -228,6 +229,58 @@ namespace BikeRental.Business.Services
                 throw new ErrorResponse((int)ResponseStatusConstants.FORBIDDEN, "Something went wrong.");
             }
             throw new ErrorResponse((int)ResponseStatusConstants.FORBIDDEN, "Email from request and the one from access token is not matched.");
+        }
+
+        public async Task<List<OwnerByAreaViewModel>> GetListOwnerByAreaId(Guid id)
+        {
+            int total = 0;
+            double? rating = 0;
+            var owners = Get(x => x.AreaId.Equals(id)).ProjectTo<OwnerByAreaViewModel>(_mapper);
+            var listOwner = owners.ToList();
+            if(listOwner.Count==0) throw new ErrorResponse((int)HttpStatusCode.BadRequest, "Can not found");
+            for (int i = 0; i < listOwner.Count;i++)
+            {
+                var listBike = await _bikeService.GetBikeByOwnerId(listOwner[i].Id);
+                if (listBike.Count != 0)
+                {
+                    listOwner[i].ListBike = _mapper.CreateMapper().Map<List<BikeViewModel>>(listBike);
+                    for (int j = 0; j < listBike.Count; j++)
+                    {
+                        var cate = await _categoryService.GetCateById(listBike[j].CategoryId);
+                        listOwner[i].ListBike[j].CategoryName = cate.Name;
+                        var brand = await _brandService.GetBrandById(cate.BrandId);
+                        listOwner[i].ListBike[j].BrandName = brand.Name;
+                        var tempRating = await _feedbackService.GetBikeRating(listBike[j].Id);
+                        if (tempRating.FirstOrDefault().Value != 0)
+                        {
+                            total += tempRating.FirstOrDefault().Key;
+                            rating += tempRating.FirstOrDefault().Value;
+                        }
+                    }
+                    if (total != 0)
+                    {
+                        listOwner[i].Rating = rating / total;
+                    }
+                    else
+                    {
+                        listOwner[i].Rating = 0;
+                    }
+                }
+            }
+            var result = listOwner.ToList();
+            for (int i = 0; i < result.Count; i++)
+            {
+                if (result[i].ListBike == null)
+                {
+                    result.RemoveAt(i);
+                    i--;
+                }
+            }
+            if (result[0].ListBike == null)
+            {
+                result.RemoveAt(0);
+            }
+            return result;
         }
     }
 }
