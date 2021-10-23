681d80f347618343ac52976fc8bc6a8a4f5e51b0
Nguyen Lam Nhat Tien <tien20001810@gmail.com>
2021-10-23 22:00:52 +0700

    Get tracking booking priority

---
+++ BikeRental.Business/Utilities/TrackingBookingUtil.cs
@@ -0,0 +1,29 @@
+﻿using BikeRental.Data.ViewModels;
+using Firebase.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeRental.Business.Utilities
+{
+    public class TrackingBookingUtil
+    {
+        public static async Task<TrackingBookingViewModel> GetTrackingBookingByDate(Guid ownerId, DateTime date)
+        {
+            string formatedDate = date.ToString("yyyy-MM-dd");
+
+            FirebaseClient firebaseClient = new FirebaseClient("https://chothuexemay-35838-default-rtdb.asia-southeast1.firebasedatabase.app/");
+
+            var trackingBooking = await firebaseClient
+                .Child("TrackingBookingPriority/" + ownerId + "/" + formatedDate)
+                .OnceSingleAsync<TrackingBookingViewModel>();
+
+            if (trackingBooking != null)
+                return await Task.Run(() => trackingBooking);
+
+            return null;
+        }
+    }
+}
---
+++ BikeRental.Data/ViewModels/TrackingBookingViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeRental.Data.ViewModels
+{
+    public class TrackingBookingViewModel
+    {
+        public int BookingTimes { get; set; }
+        public int DeniedTimes { get; set; }
+    }
+}
