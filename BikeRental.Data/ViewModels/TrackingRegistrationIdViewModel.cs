8a170cc3f4016e210137fef6076156a55e97ebee
Nguyen Lam Nhat Tien <tien20001810@gmail.com>
2021-10-24 21:56:41 +0700

    Get registrationId of owner

---
+++ BikeRental.Data/ViewModels/TrackingRegistrationIdViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeRental.Data.ViewModels
+{
+    public class TrackingRegistrationIdViewModel
+    {
+        public string RegistrationId { get; set; }
+    }
+}
